8cc9191386ec284a685d70a4ec59ef1ca6f6304e
郭正中 <guozhengzhong@tal.com>
2022-04-27 16:20:30 +0800

    如何将处理后的 data 页面可以引用

--- src/state.js
+++ src/state.js
@@ -12,9 +12,10 @@ export function initState(vm) {
 function initData(vm) {
   let data = vm.$options.data;
   // data 允许传入函数和对象两种形式，因此需要判断
-  data = typeof data === "function" ? data.call(vm) : data;
+  data = vm._data = typeof data === "function" ? data.call(vm) : data;
   // 属性劫持
   observe(data);
   // 经过 observe 处理后的 data 都被观测过了，拥有了 get 和 set
   console.log(data);
+  // 但是现在的这个 data 是响应式化处理后的，页面里是直接获取不到的，想要获取，还是得挂到 vm 上
 }
