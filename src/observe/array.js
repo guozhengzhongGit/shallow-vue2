12536a9fd2ccc49defb4eed60fb6de312f755ef8
郭正中 <guozhengzhong@tal.com>
2022-04-27 18:41:16 +0800

    数组类型数据的响应式化

---
+++ src/observe/array.js
@@ -0,0 +1,40 @@
+let oldArrayPrototype = Array.prototype;
+// arrPrototype.__proto__ = oldArrayPrototype;
+let arrPrototype = Object.create(oldArrayPrototype);
+const methods = [
+  "push",
+  "pop",
+  "shift",
+  "unshift",
+  "sort",
+  "splice",
+  "reverse",
+];
+methods.forEach((method) => {
+  // 调用这个方法的，就是某个数组数据，即 data 里的某一个成员
+  arrPrototype[method] = function (...args) {
+    console.log("劫持的数组方法");
+    let inserted;
+    const ob = this.__ob__;
+    switch (method) {
+      case "push":
+      case "unshift":
+        inserted = args;
+        break;
+      case "splice":
+        inserted = args.slice(2);
+        break;
+      default:
+        break;
+    }
+    if (inserted) {
+      // 对新增的数据也要进行观测，此时需要调用 index.js 里的 observeArr 方法，但这个方法是存在另一个文件的类里的，怎么办
+      // 因为这里的 this 就是 data 里的某个数据，所以已经是经过 observe 处理过的了，即 new Observer 的产物，所以
+      ob.observeArr(inserted);
+      console.log(this);
+    }
+    return oldArrayPrototype[method].call(this, ...args); // 此处的 this 是谁，谁调用的这个方法，就是谁，比如外部肯定是用户写一个 [].push(1)那么 this 就是这个数组自己
+  };
+});
+
+export default arrPrototype;
--- src/observe/index.js
+++ src/observe/index.js
@@ -1,6 +1,23 @@
+import arrPrototype from "./array";
 class Observer {
   constructor(data) {
-    this.walk(data);
+    Object.defineProperty(data, "__ob__", {
+      value: this,
+      enumerable: false,
+    });
+    // 如果是数组的话使用 defineProperty 会浪费很多性能，不推荐这么做
+    // 改写数组方法，如果使用了可以改变数组的 api，就去劫持这个方法；变异方法：push、pop、shift、unshift、sort、splice、reverse
+    // 修改数组的索引和长度无法更新视图
+    if (Array.isArray(data)) {
+      data.__proto__ = arrPrototype;
+      // 数组里的某一项也可能是对象，所以也需要去观测
+      this.observeArr(data);
+    } else {
+      this.walk(data);
+    }
+  }
+  observeArr(data) {
+    data.forEach((item) => observe(item));
   }
   walk(data) {
     let keys = Object.keys(data);
@@ -12,6 +29,7 @@ class Observer {
 }
 
 function defineReactive(data, key, value) {
+  observe(value); // 递归劫持属性，data 里传的对象可能不止一层
   // 属性会全部被劫持重写增加了 get 和 set
   Object.defineProperty(data, key, {
     get() {
@@ -19,6 +37,7 @@ function defineReactive(data, key, value) {
     },
     set(newValue) {
       if (newValue !== value) {
+        observe(newValue); // 赋予的新值也要观测
         value = newValue;
       }
     },
@@ -28,6 +47,7 @@ function defineReactive(data, key, value) {
 export function observe(data) {
   if (!data || typeof data !== "object") return;
   // 对数据进行响应式化
+  if (data.__ob__) return data;
   // 如果一个对象已经被观测过，就不用再次观测了，需要加一个标识 __ob__
   return new Observer(data);
 }
